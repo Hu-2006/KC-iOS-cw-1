ca0196c9bcca3466c4225609086a33bc4ad1bc69
Hu-2006 <hussain.bo.ali.q8@gmail.com>
2022-10-10 14:51:14 +0300

    Added an xcode project

---
+++ katkoot/katkoot/katkootApp.swift
@@ -0,0 +1,17 @@
+//
+//  katkootApp.swift
+//  katkoot
+//
+//  Created by Mac on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct katkootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
